f834d112a12c45c2514af3e471c5e0c488a947ff
succip <succip@gmail.com>
2021-08-12 21:12:44 -0700

    webpack fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,30 @@
-const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const { cleanData } = require('jquery');
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist'
-    },
-    output: {
-        path: path.join(__dirname, 'dist'),
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            }
-        ],
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            title: 'GeoJSON Creator',
-            template: './public/index.html',
-            filename: './index.html'
-        })
-    ]
-}
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      title: "GeoJSON Creator",
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
