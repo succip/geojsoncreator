165b73ee16afed853480965d0f1bcb6c5672203e
succip <succip@gmail.com>
2021-08-12 21:09:16 -0700

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { cleanData } = require('jquery');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            title: 'GeoJSON Creator',
+            template: './public/index.html',
+            filename: './index.html'
+        })
+    ]
+}
